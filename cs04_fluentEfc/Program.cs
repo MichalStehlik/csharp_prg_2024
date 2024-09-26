f67dd4c27e98b94b6c73d67355b5a989ed86c327
Michal Stehlík <michal.stehlik@pslib.cz>
2024-09-26 10:05:29 +0200

    aggregation

--- cs04_fluentEfc/Program.cs
+++ cs04_fluentEfc/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using cs04_fluentEfc.Data;
+using cs04_fluentEfc.Models;
+
 Console.WriteLine("Hello, World!");
+AppDbContext db = new AppDbContext();
+db.Users.Add(new User { Firstname = "Charlie", Lastname = "Brown" });
+db.SaveChanges();
+foreach (var r in db.Records.GroupBy(x => x.UserId).ToList())
+{
+    Console.WriteLine(r.Key.ToString() + " " + r.Count());
+}
