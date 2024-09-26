89530ffbc22b822a727af640d908380dc3e11b62
Michal Stehlík <michal.stehlik@pslib.cz>
2024-09-26 10:28:11 +0200

    dapper

---
+++ cs05_dapper/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace cs05_dapper.Models
+{
+    internal class User
+    {
+        public Guid UserId { get; set; }
+        public required string Firstname { get; set; }
+        public required string Lastname { get; set; }
+    }
+}
---
+++ cs05_dapper/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using cs05_dapper.Models;
+using Dapper;
+using System.Data.SqlClient;
+
+string cs = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Tasks01;Integrated Security=True;Connect Timeout=30";
+using (var conn = new SqlConnection(cs))
+{
+    conn.Open();
+    var version = conn.ExecuteScalar<string>("SELECT @@VERSION");
+    Console.WriteLine(version);
+    // INSERT
+    var q = "INSERT INTO Users (UserId, Firstname, Lastname) Values ('" + Guid.NewGuid() + "', 'Richard','Black')";
+    int result = conn.Execute(q);
+    Console.WriteLine($"Inserted {result} rows");
+    // INSERT with parameters
+    var u = new User { UserId = Guid.NewGuid(), Firstname = "Francis", Lastname = "Daniels" };
+    q = "INSERT INTO Users (UserId, Firstname, Lastname) Values (@UserId, @Firstname, @Lastname)";
+    result = conn.Execute(q, u);
+    Console.WriteLine($"Inserted {result} rows");
+    // SELECT
+    var users = conn.Query<User>("SELECT * FROM Users");
+    foreach (var user in users)
+    {
+        Console.WriteLine($"{user.UserId} {user.Firstname} {user.Lastname}");
+    }
+}
